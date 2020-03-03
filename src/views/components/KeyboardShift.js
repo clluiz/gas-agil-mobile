328563587e64d3291e9988c269fcf05cfbf7885b
Cleiton Luiz <cleiton.vanquish@gmail.com>
2020-03-03 20:38:13 -0300

    Created component to handle bug when keyboard covers an input at the bottom of a screen

---
+++ src/views/components/KeyboardShift.js
@@ -0,0 +1,95 @@
+/**
+ * Solução baseada em: https://codeburst.io/react-native-keyboard-covering-inputs-72a9d3072689
+ * Criada a propriedade extraGap para adicionar um espaço extra na parte inferior do componente
+ */
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import { 
+  Animated, 
+  Dimensions, 
+  Keyboard, 
+  StyleSheet, 
+  TextInput, 
+  UIManager 
+} from 'react-native';
+
+const { State: TextInputState } = TextInput;
+
+export default class KeyboardShift extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      shift: new Animated.Value(0),
+    }
+    this.keyboardDidShowSub = Keyboard.addListener('keyboardDidShow', this.handleKeyboardDidShow);
+    this.keyboardDidHideSub = Keyboard.addListener('keyboardDidHide', this.handleKeyboardDidHide);
+  }
+
+  componentWillUnmount() {
+    this.keyboardDidShowSub.remove();
+    this.keyboardDidHideSub.remove();
+  }
+
+  render() {
+    const { children: renderProp } = this.props;
+    const { shift } = this.state;
+    return (
+      <Animated.View style={[styles.container, { transform: [{translateY: shift}] }]}>
+        {renderProp()}
+      </Animated.View>
+    );
+  }
+
+  handleKeyboardDidShow = (event) => {
+    const { height: windowHeight } = Dimensions.get('window');
+    const keyboardHeight = event.endCoordinates.height;
+    const currentlyFocusedField = TextInputState.currentlyFocusedField();
+    UIManager.measure(currentlyFocusedField, (originX, originY, width, height, pageX, pageY) => {
+      const fieldHeight = height;
+      const fieldTop = pageY;
+      const gap = (windowHeight - (keyboardHeight + this.props.extraGap)) - (fieldTop + fieldHeight);
+      if (gap >= 0) {
+        return;
+      }
+      Animated.timing(
+        this.state.shift,
+        {
+          toValue: gap,
+          duration: 1000,
+          useNativeDriver: true,
+        }
+      ).start();
+    });
+  }
+
+  handleKeyboardDidHide = () => {
+    Animated.timing(
+      this.state.shift,
+      {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }
+    ).start();
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    left: 0,
+    position: 'absolute',
+    top: 0,
+    width: '100%'
+  }
+});
+
+KeyboardShift.propTypes = {
+  children: PropTypes.func.isRequired,
+  extraGap: PropTypes.number
+};
+
+KeyboardShift.defaultProps = {
+  extraGap: 0
+}
--- src/views/pages/EditUser.js
+++ src/views/pages/EditUser.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import {
-  Text,
   StyleSheet,
+  Text,
   View,
 } from 'react-native'
 import PageHeader from '../components/PageHeader'
-import { Formik } from 'formik'
-import { TextInput, MaskedTextInput } from '../components/inputs'
-import { COLORS } from '../../styles'
+import {
+  Formik
+} from 'formik'
+import {
+  MaskedTextInput,
+  TextInput
+} from '../components/inputs'
+import {
+  COLORS
+} from '../../styles'
 import {
   Button
 } from 'react-native-elements'
-
+import KeyboardShift from '../components/KeyboardShift';
 
 const styles = StyleSheet.create({
   form: {
@@ -32,49 +39,55 @@ const styles = StyleSheet.create({
 
 export default (props) => {
 
-  return(<View>
-    <PageHeader navigation={props.navigation} />
-    <Text style={styles.title}>Preencha seus dados pessoais</Text>
-    <Formik
-      initialValues={{ name: '', email: '', phone: '', address: '', password: '', confirmPassword: '' }}
-      onSubmit={values => console.log(values)}
-    >
-      {({ handleChange, handleBlur, handleSubmit, values }) => (
-        <View style={styles.form}>
-          <TextInput
-            onChangeText={handleChange('name')}
-            onBlur={handleBlur('name')}
-            value={values.name}
-            placeholder="Nome"
-          />
-          <TextInput
-            onChangeText={handleChange('email')}
-            onBlur={handleBlur('email')}
-            value={values.email}
-            placeholder="Email"
-          />
-          <MaskedTextInput 
-            onChangeText={handleChange('phone')}
-            placeholder="Telefone celular"
-            mask={'([00]) [0] [0000] [0000]'}
-          />
-          <TextInput
-            onChangeText={handleChange('password')}
-            onBlur={handleBlur('password')}
-            value={values.password}
-            placeholder="Senha"
-            secureTextEntry={true}
-          />    
-          <TextInput
-            onChangeText={handleChange('confirmPassword')}
-            onBlur={handleBlur('confirmPassword')}
-            value={values.confirmPassword}
-            placeholder="Confirme sua senha"
-            secureTextEntry={true}
-          />                         
-          <Button buttonStyle={styles.confirmButton} onPress={handleSubmit} title="Confirmar" />
-        </View>
-      )}
-    </Formik>
-  </View>)
+  return (<KeyboardShift extraGap={100}>
+    {
+      () => (
+        <>
+          <PageHeader navigation={props.navigation} />
+          <Text style={styles.title}>Preencha seus dados pessoais</Text>
+          <Formik
+            initialValues={{ name: '', email: '', phone: '', address: '', password: '', confirmPassword: '' }}
+            onSubmit={values => console.log(values)}
+          >
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+              <View style={styles.form}>
+                <TextInput
+                  onChangeText={handleChange('name')}
+                  onBlur={handleBlur('name')}
+                  value={values.name}
+                  placeholder="Nome"
+                />
+                <TextInput
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                  placeholder="Email"
+                />
+                <MaskedTextInput
+                  onChangeText={handleChange('phone')}
+                  placeholder="Telefone celular"
+                  mask={'([00]) [0] [0000] [0000]'}
+                />
+                <TextInput
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                  placeholder="Senha"
+                  secureTextEntry={true}
+                />
+                <TextInput
+                  onChangeText={handleChange('confirmPassword')}
+                  onBlur={handleBlur('confirmPassword')}
+                  value={values.confirmPassword}
+                  placeholder="Confirme sua senha"
+                  secureTextEntry={true}
+                />
+                <Button buttonStyle={styles.confirmButton} onPress={handleSubmit} title="Confirmar" />
+              </View>
+            )}
+          </Formik>
+        </>
+      )
+    }
+  </KeyboardShift>)
 }
